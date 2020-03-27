f61a62d9c716f501adc9ff5d70883cf9049ff086
ghadabs <ghadabensalem0@gmail.com>
2020-03-27 16:08:10 +0100

    contact et newsletter

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -4,16 +4,44 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CompanyRepository;
+use App\Entity\Company;
+use App\Entity\NewsLetter;
+use App\Repository\NewsLetterRepository;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 
 class AboutController extends AbstractController
 {
     /**
      * @Route("/about", name="about")
      */
-    public function index()
+    public function index(CompanyRepository $repository, Request $request)
     {
-        return $this->render('about/about.html.twig', [
-            'controller_name' => 'AboutController',
-        ]);
+
+        $company=$this->getDoctrine()->getRepository(Company::class)->findAll();
+
+        $contact = new NewsLetter;     
+        # Add form fields
+          $form = $this->createFormBuilder($contact)
+          ->add('email', EmailType::class, array('label'=> 'Email','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+          ->getForm();
+        # Handle form response
+          $form->handleRequest($request);
+  
+          if($form->isSubmitted() &&  $form->isValid()){
+              $this->addFlash('success','You are subscribed!');
+              $email = $form['email']->getData();
+        # set form data   
+              $contact->setEmail($email);                             
+         # finally add data in database
+              $sn = $this->getDoctrine()->getManager();      
+              $sn -> persist($contact);
+              $sn -> flush();
+      return $this->redirectToRoute("about");   
+      }
+        return $this->render('about/about.html.twig',  ['companies'=> $company,'form' => $form->createView()]);
     }
 }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,5 +1,4 @@
 <?php
-// src/Controller/RegistrationController.php
 namespace App\Controller;
 
 use App\Form\UserType;
@@ -8,6 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\NewsLetter;
+use App\Repository\NewsLetterRepository;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 
 class RegisterController extends Controller
 {
@@ -16,13 +20,32 @@ class RegisterController extends Controller
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        $contact = new NewsLetter;     
+        # Add form fields
+          $form = $this->createFormBuilder($contact)
+          ->add('email', EmailType::class, array('label'=> 'Email','attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+          ->getForm();
+        # Handle form response
+          $form->handleRequest($request);
+  
+          if($form->isSubmitted() &&  $form->isValid()){
+              $this->addFlash('success','You are subscribed!');
+              $email = $form['email']->getData();
+        # set form data   
+              $contact->setEmail($email);                             
+         # finally add data in database
+              $sn = $this->getDoctrine()->getManager();      
+              $sn -> persist($contact);
+              $sn -> flush();
+      return $this->redirectToRoute("register");   
+      }
         // 1) build the form
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $form1 = $this->createForm(UserType::class, $user);
 
         // 2) handle the submit (will only happen on POST)
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $form1->handleRequest($request);
+        if ($form1->isSubmitted() && $form1->isValid()) {
 
             // 3) Encode the password (you could also do this via Doctrine listener)
             $password = $passwordEncoder->encodePassword($user, $user->getPassword());
@@ -41,7 +64,7 @@ class RegisterController extends Controller
 
         return $this->render(
             'register/register.html.twig',
-            array('form' => $form->createView())
+            array('form1' => $form1->createView(),'form' => $form->createView())
         );
     }
 }
